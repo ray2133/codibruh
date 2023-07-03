b4c535e4a4ff801a88c2b2744aa5c2a7ce41e0ba
ray2133 <2102071@utrivieramaya.edu.mx>
2023-07-02 23:15:13 -0600

    añadir

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  constructor(private barcodeScanner: BarcodeScanner) { }
+
+  ngOnInit() {
+
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+     }).catch(err => {
+         console.log('Error', err);
+     });
+
+  }
+/*
+  scan(){
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+     }).catch(err => {
+         console.log('Error', err);
+     });
+  }
+*/
+}
